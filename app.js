0babc5c2b3ab1a456303debbf0836449fa032898
vinaypatel1437 <66087753+vinaypatel1437@users.noreply.github.com>
2023-01-28 05:11:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fileUpload = require('express-fileupload');
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -16,15 +15,6 @@ app.use(cors());
 app.use(gmailRouter);
 app.use(formsRouter);
 
-app.post("/pdftohtml", fileUpload(), async (req, res) => {
-  const sampleFile = req.files.uploadedFile;
-  const html = await pdf2html.html(sampleFile);
-  // do something with file
-  res.send({
-    message: 'File converted success',
-    data: html,
-  });
-});
 
 // app.post("/htmltopdf", async (req, res) => {
 //   const sampleFile = req.files.uploadedFile;
