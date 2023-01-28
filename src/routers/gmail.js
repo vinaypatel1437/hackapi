0441e114264442bbdfbcb77fec012e1fa44ba1c1
vinaypatel1437 <66087753+vinaypatel1437@users.noreply.github.com>
2023-01-28 22:08:05 +0530

    Update gmail.js

--- src/routers/gmail.js
+++ src/routers/gmail.js
@@ -5,15 +5,15 @@ const nodemailer = require("nodemailer");
 const Users = require("../models/gmail");
 const Emails = require("../models/emails");
 
-var transport = nodemailer.createTransport({
-    host: 'smtp.zoho.eu',
-    port: 465,
-    secure: true, //ssl
-    auth: {
-        user:process.env.EMAIL,
-        pass:process.env.EMAIL_PASSWORD
-    }
-});
+// var transport = nodemailer.createTransport({
+//     host: 'smtp.zoho.in',
+//     port: 465,
+//     secure: true, //ssl
+//     auth: {
+//         user:process.env.EMAIL,
+//         pass:process.env.EMAIL_PASSWORD
+//     }
+// });
 
 
 router.post("/gmail/signup", async (req, res) => {
@@ -33,29 +33,29 @@ router.post("/gmail/signup", async (req, res) => {
 router.post("/gmail/sendemail", async (req, res) => {
   try {
     //Integrate NodeMailer
-    var mailOptions = {
-        from: process.env.EMAIL,
-        to: req.body.senderEmail,
-        subject: req.body.subject,
-        html: req.body.html,
-    };
-    let info = transport.sendMail(mailOptions);
-    var users = await Email.find({
+    // var mailOptions = {
+    //     from: process.env.EMAIL,
+    //     to: req.body.senderEmail,
+    //     subject: req.body.subject,
+    //     html: req.body.html,
+    // };
+    // let info = transport.sendMail(mailOptions);
+    var users = await Emails.find({
       senderEmail: req.body.senderEmail,
     });
     //users = JSON.parse(users);
-    if (!users.length) {
-      var user = new Email({
+    if (!users.length || users === {}) {
+      var user = new Emails({
         senderEmail: req.body.senderEmail,
         email: req.body.email,
-        emailData: JSON.stringify([`${req.body.subject} ${req.body.html}]),
+        emailData: JSON.stringify([`${req.body.subject} ${req.body.html}`]),
       });
       var createUser = await user.save();
       console.log(createUser);
     } else {
-      console.log(users[0].message, "Message");
+      console.log(users[0].emailData, "Message");
       const messages = [...JSON.parse(users[0].emailData), `${req.body.subject} ${req.body.html}`];
-      await Email.updateOne(
+      await Emails.updateOne(
         { senderEmail: req.body.senderEmail},
         {
           $set: {
@@ -66,7 +66,7 @@ router.post("/gmail/sendemail", async (req, res) => {
     }
     res.status(201).send({
       message: 'Email sent successfully',
-      success: info,
+      success: createUser,
     });
   } catch (e) {
     res.status(400).send(e);
@@ -90,7 +90,10 @@ router.post("/gmail/login", async (req, res) => {
   try {
     let user = {};
     const userData = await Users.find({email : req.body.email});
-    if(userData[0].pass===req.body.pass) {
+    if (userData.length === 0 || !userData) {
+      res.status(400).send({ message: "Email incorrect"});
+    }
+  else if(userData[0].pass===req.body.pass) {
         res.status(200).send({
         user: userData[0],
         message: "Success"
@@ -99,9 +102,8 @@ router.post("/gmail/login", async (req, res) => {
     else {
         res.status(400).send({message: 'Password incorrect'});
     }
-    if (userData.length === 0) {
-      res.status(400).send("Email incorrect");
-    }
+    console.log(userData)
+    
   } catch (e) {
     res.status(400).send(e);
   }
