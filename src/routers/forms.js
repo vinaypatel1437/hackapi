1dfa30437a92a5204840a0be5ff50caad7d24033
vinaypatel1437 <66087753+vinaypatel1437@users.noreply.github.com>
2023-01-28 22:13:25 +0530

    Update forms.js

--- src/routers/forms.js
+++ src/routers/forms.js
@@ -58,23 +58,21 @@ router.post("/forms/getalldata", async (req, res) => {
 
 router.post("/forms/login", async (req, res) => {
   try {
-    let user = {};
-    const userData = await Forms.find();
-    for (let i = 0; i < userData.length; i++) {
-      const ele = userData[i];
-      if (ele.email == req.body.email) {
-        if (ele.pass == req.body.pass) {
-          user = ele;
-          res.status(200).send({
-            message: "User login success",
-            user: ele,
-          });
-        }
-      }
+    const userData = await Forms.find({email : req.body.email});
+    if (userData.length === 0 || !userData) {
+      res.status(400).send({ message: "Email incorrect"});
     }
-    if (!user) {
-      res.status(400).send("Email or password incorrect.");
+  else if(userData[0].pass===req.body.pass) {
+        res.status(200).send({
+        user: userData[0],
+        message: "User login success"
+      });
     }
+    else {
+        res.status(400).send({message: 'Password incorrect'});
+    }
+    console.log(userData)
+    
   } catch (e) {
     res.status(400).send(e);
   }
